2a533e957ba52b78372a9b8203bce3af66e80163
Furkan Çakmak <baruta9898@gmail.com>
2018-11-17 05:25:01 +0300

    İlk Version Yüklendi

---
+++ Assets/Kodlar/OyunKontrol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyunKontrol : MonoBehaviour {
+
+	public GameObject asteroid;
+	public Vector3 randomPos;
+	void Start () {
+		olustur();
+	}
+	
+	void olustur(){
+Vector3 vec=new Vector3(Random.Range(-randomPos.x,randomPos.x),0,randomPos.z);
+Instantiate(asteroid,vec,Quaternion.identity);
+
+	}
+	
+}
---
+++ Assets/Kodlar/TemaslaYokOl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemaslaYokOl : MonoBehaviour
+ {
+public GameObject patlama;
+public GameObject playerPatlama;
+void OnTriggerEnter(Collider col)
+{
+	if (col.tag!="Sinir")
+	{
+		Destroy(col.gameObject);
+	Destroy (gameObject);
+	Instantiate (patlama,transform.position,transform.rotation);
+	}
+	if (col.tag=="Player")
+	{
+		Instantiate (playerPatlama,col.transform.position,col.transform.rotation);
+	}
+}
+	
+	
+}
